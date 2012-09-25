d6868aa08e7e706162b0e73d7323f5123ba8b581
Jari Aalto <jari.aalto@cante.net>
2012-09-25 17:02:47 +0300

    test/test.sh: new

---
+++ test/test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+TMPBASE=$TMPDIR/tmp.$$
+CURDIR=$0
+CURDIR=${CURDIR%/*}
+
+TMPDIR=${TMPDIR:-/tmp}
+
+AtExit ()
+{
+  rm -f "$TMPBASE"*
+}
+
+Run ()
+{
+    echo "$*"
+    shift
+    eval "$@"
+}
+
+trap AtExit 0 1 2 3 15
+
+# #######################################################################
+
+file="$TMPBASE/pic-test.png"
+
+Run "%% TEST bmp2png:"  bmp2png "$CURDIR/pic.bmp" -o "$file"
+
+Run "%% TEST png2bmp:" png2bmp "$file"
+
+# End of file
